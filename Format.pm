8ee846716960fd42c10e65e2086817f4ebd33638
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-02-15 00:13:48 +0100

    Prepare for 0.03 version

--- Format.pm
+++ Format.pm
@@ -8,7 +8,7 @@ use Class::Utils qw(set_params);
 use Error::Pure qw(err);
 use Getopt::Std;
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -163,6 +163,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
